e87b75610a0db23407743699aa7a2476ca6e530e
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-30 13:37:44 -0300

    feat: categories module

---
+++ api/src/modules/categories/categories.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from './services/categories.service';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
+// import { CreateCategoryDto } from './dto/create-category.dto';
+// import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  findAll(@ActiveUserId() userId: string) {
+    return this.categoriesService.findAllByUserId(userId);
+  }
+
+  // @Post()
+  // create(@Body() createCategoryDto: CreateCategoryDto) {
+  //   return this.categoriesService.create(createCategoryDto);
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.categoriesService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateCategoryDto: UpdateCategoryDto,
+  // ) {
+  //   return this.categoriesService.update(+id, updateCategoryDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.categoriesService.remove(+id);
+  // }
+}
