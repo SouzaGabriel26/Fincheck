18015cec85eda66df68556c88f1d65ea05f365f3
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-30 13:40:35 -0300

    feat: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3000);
 }
