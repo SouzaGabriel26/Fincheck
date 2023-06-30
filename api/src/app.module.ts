256ab70e65e73c713bd7bca72a7cc3df970f1e42
Gabriel Alves <gabriel.asouza2608@hotmail.com>
2023-06-30 13:27:05 -0300

    feat: adding modules to app.module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
-import { DatabaseModule } from './shared/database/database.module';
-import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
+
+import { DatabaseModule } from './shared/database/database.module';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+    CategoriesModule,
+    BankAccountsModule,
+    TransactionsModule,
+  ],
   controllers: [],
   providers: [
     {
